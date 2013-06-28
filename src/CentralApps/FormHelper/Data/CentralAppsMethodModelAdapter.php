561df3509dd6ab84a74e4053dd4e09eeec6bdeb7
Michael Peacock <mkpeacock@gmail.com>
2013-06-28 10:05:22 +0100

    Files for implementation for specific integration options (CA DAO and Symfony Validator)

---
+++ src/CentralApps/FormHelper/Data/CentralAppsMethodModelAdapter.php
@@ -0,0 +1,9 @@
+<?php
+namespace CentralApps\FormHelper\Data;
+
+class CentralAppsMethodModelAdapter extends FormData
+{
+	// TODO: implement
+
+	// TODO: consider implications of things which have many items e.g. invoice items
+}
---
+++ src/CentralApps/FormHelper/Errors/SymfonyValidatorViolationsAdapter.php
@@ -0,0 +1,11 @@
+<?php
+namespace CentralApps\FormHelper\Errors;
+
+class SymfonyValidatorViolationsAdapter extends FormErrors
+{
+	// TODO: implement
+
+	// TODO: need to investigate what happens when a field has multiple violations
+
+	// TODO: need to consider violation loops e.g. invoice items in an invoice
+}
